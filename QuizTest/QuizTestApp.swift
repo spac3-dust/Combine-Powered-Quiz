88b3159a945d73192ddc78b6fc79328bacbb883b
Abe Park <abepark013@gmail.com>
2023-04-07 19:00:17 -0400

    Initial Commit

---
+++ QuizTest/QuizTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizTestApp.swift
+//  QuizTest
+//
+//  Created by Abe on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
